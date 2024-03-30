528c3acf7ca8c5dc75e94c7c618a3d64d5ff0730
FeHunter <hunterfe1111@gmail.com>
2024-03-30 19:35:23 -0300

    Ajuste visual da área de tira foto.

--- screens/Tabs/Gallary/GallaryTakePicTab.js
+++ screens/Tabs/Gallary/GallaryTakePicTab.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Pressable, Image, FlatList, StyleSheet, Platform } from 'react-native';
+import { View, Text, Pressable, Image, FlatList, StyleSheet, Dimensions } from 'react-native';
 import { Camera } from 'expo-camera';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import firebaseRoutes from '../../../assets/FirebaseRoutes';
@@ -13,7 +13,7 @@ export function GallaryTakePicTab() {
   const [camera, setCamera] = useState(null);
   const [photoUri, setPhotoUri] = useState(null);
   const [photos, setPhotos] = useState([]);
-  const [status, setStatus] = useState('');
+  const [status, setStatus] = useState('---');
 
   useEffect(() => {
     getPhotos();
@@ -36,6 +36,10 @@ export function GallaryTakePicTab() {
       const firebaseStorage = getStorage(app);
       const photoRef = ref(firebaseStorage, `Gallary Pictures ${new Date().getTime()}.png`);
       await uploadString(photoRef, photo, "data_url");
+      setStatus("Foto salva");
+      setTimeout(() => {
+        setStatus("---");
+      }, 1000);
     }catch (error){
       console.log(error.message);
     }
@@ -59,6 +63,7 @@ export function GallaryTakePicTab() {
       setPhotoUri(picture.uri);
       setPhotos(prevPhotos => [...prevPhotos, picture.uri]);
       savePhoto(picture.uri);
+      setStatus("Salvando foto...");
     }
   };
 
@@ -68,100 +73,43 @@ export function GallaryTakePicTab() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Camera</Text>
       <Camera 
         style={styles.cameraView}
         type={type}
         ref={ref => setCamera(ref)}
       />
       <Pressable style={styles.button} onPress={takePicture}>
-        <Text style={styles.buttonText}>Tirar foto</Text>
-        <Icon name='camera-retro' size={20} color={'white'} />
+        <Icon name='camera-retro' size={50} color={'white'} />
       </Pressable>
-      <View style={styles.gallary}>
-        <Text style={styles.title}>Fotos {status}</Text>
-        <FlatList
-          data={photos}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => (
-            <Image source={{ uri: item }} style={styles.imageGallery} />
-          )}
-        />
-      </View>
+      <Text style={styles.status}>{status}</Text>
     </View>
   );
 }
 
+const windowWidth = Dimensions.get('window').width;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '100%',
-    height: 200,
-  },
-  title: {
-    fontSize: 16,
-    textAlign: 'center',
-    marginVertical: 5,
+    height: "100%",
+    alignItems: 'center',
   },
   cameraView: {
-    flex: 1,
-    width: '100%',
-    height: 100,
-    borderWidth: 2,
-    borderColor: '#556b2f',
+    width: windowWidth * 1,
+    height: windowWidth * 0.3 * (8 / 16), // Mantém a proporção 16:8
     resizeMode: 'contain',
   },
-  lastPic: {
+  status: {
+    fontSize: 18,
+    padding: 5,
+    color: 'gray',
+  },
+  button: {
     width: '100%',
-    height: 200,
-    resizeMode: 'contain',
-  },
-  buttonText: {
-    fontSize: 20,
-    color: 'white'
-  },
-  gallary: {
-    width: '100%',
-    height: 450,
-  },
-  imageGallery: {
-    width: '100%',
-    height: 400,
-    resizeMode: 'contain',
-  },
-  ...Platform.select({
-    android: {
-      button: {
-        width: '100%',
-        paddingVertical: 10,
-        paddingHorizontal: 30,
-        backgroundColor: '#556b2f',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      }
-    },
-    ios: {
-      button: {
-        width: '100%',
-        paddingVertical: 10,
-        paddingHorizontal: 30,
-        backgroundColor: '#191970',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      }
-    },
-    web: {
-      button: {
-        width: '100%',
-        paddingVertical: 10,
-        paddingHorizontal: 30,
-        backgroundColor: '#800020',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      }
-    }
-  })
+    height: "10%",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  }
 });
